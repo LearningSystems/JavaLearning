aea8b2f2c69795720a43d7d0c11a07a55b0289ff
Sreedhar Bukya <sreedhar@kisanhub.com>
2015-06-30 08:33:38 +0530

    Added calculator  class

---
+++ src/com/javalearning/helloworld/com/javalearning/calculations/Calculator.java
@@ -0,0 +1,19 @@
+package com.javalearning.helloworld.com.javalearning.calculations;
+
+public class Calculator {
+
+    public int add(int var1, int var2) {
+        return var1 + var2;
+    }
+
+    public static void main(String[] args) {
+        int var1 = 5;
+        int var2 = 6;
+
+        Calculator calculator = new Calculator();
+
+        int result = calculator.add(var1, var2);
+        System.out.println(result);
+
+    }
+}
